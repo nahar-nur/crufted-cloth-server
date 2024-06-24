855c62731f40714d4bcfa1b4e0f760cf2a79e2f4
nahar-nur <naharbb@gmail.com>
2024-06-24 23:12:37 +0600

    data storage in the mongodb

--- index.js
+++ index.js
@@ -30,6 +30,14 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+    const craftCollection = client.db('craftDB').collection('craft')
+
+    app.post('/craft', async(req,res)=>{
+        const newCraft = req.body;
+        console.log(newCraft);
+        const result = await craftCollection.insertOne(newCraft)
+        res.send(result)
+    })
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
